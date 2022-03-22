5b8d04e56cbcfb51a0d5b032efddb53a70a20c7a
Joseph Ballschneider <jballschneider@yext.com>
2022-03-22 17:10:34 -0400

    fix build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,3 @@
 curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.17.3
 export PATH="$HOME/.deno/bin:$PATH"
-deno bundle --config tsconfig.json sites-plugins/reactPlugin/index.tsx assets/bundle.js
+deno bundle --config tsconfig.json src/index.tsx assets/bundle.js
