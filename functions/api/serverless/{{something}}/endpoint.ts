9981146e11ec601c87ae706e1443b03fa30d6c37
jballschneider <jballschneider@yext.com>
2022-08-05 19:14:57 +0000

    Real Serverless Func

--- functions/api/serverless/{{something}}/endpoint.ts
+++ functions/api/serverless/{{something}}/endpoint.ts
@@ -1,1 +1,35 @@
-// Eventually it would be great if this logged something
+export async function main(argumentJson) {
+	let catResp;
+	if (argumentJson["urlArgs"]["something"]) {
+		console.log(argumentJson["urlArgs"]["something"])
+	} 
+
+	return {
+		"body": `		
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>
+body{
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	font-family: monospace;
+	font-size: 32px;
+}
+</style>
+</head>
+<body>
+<h1>Dynamic Routing Demo! 🐈</h1>
+<p>You're variadic argument is ${argumentJson["urlArgs"]["something"]}</p>
+</body>
+</html>
+		`,
+		"statusCode": 200,
+		"headers": {
+			// "Cache-control": "private",
+			"X-Yext-Test": "Example header",
+		},
+	  };
+}
