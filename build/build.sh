cad8089b861ec1a06e103879669871831de8ea22
jballschneider <jballschneider@yext.com>
2022-08-09 16:11:19 +0000

    make redirects.csv in build command

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,6 @@
 curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.18.0
 export PATH="$HOME/.deno/bin:$PATH"
 deno bundle --config tsconfig.json src/index.tsx assets/bundle.js
+
+touch sites.config/redirects.csv
+echo "home.html,index.html" > sites.config/redirects.csv
