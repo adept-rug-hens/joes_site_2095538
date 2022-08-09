f650ccb639ac58233aacc618fef94f402ae305b2
jballschneider <jballschneider@yext.com>
2022-08-09 16:13:13 +0000

    Fix redirects generating command

--- build/build.sh
+++ build/build.sh
@@ -2,5 +2,5 @@ curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.18.0
 export PATH="$HOME/.deno/bin:$PATH"
 deno bundle --config tsconfig.json src/index.tsx assets/bundle.js
 
-touch sites.config/redirects.csv
-echo "home.html,index.html" > sites.config/redirects.csv
+touch sites-config/redirects.csv
+echo "home.html,index.html" > sites-config/redirects.csv
