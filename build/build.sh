0ee87d9f5b5fe5fbe275d1eed59dd146df161df9
nramachandran <nramachandran@yext.com>
2022-08-10 21:08:12 +0000

    recreating error

--- build/build.sh
+++ build/build.sh
@@ -3,6 +3,6 @@ export PATH="$HOME/.deno/bin:$PATH"
 deno bundle --config tsconfig.json src/index.tsx assets/bundle.js
 
 touch sites-config/redirects.csv
-echo "/home.html,/index.html" > sites-config/redirects.csv
+echo "home.html,index.html" > sites-config/redirects.csv
 ls -al sites-config
 cat sites-config/redirects.csv
