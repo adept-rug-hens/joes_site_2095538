0ba907da39c3ba8bfb70be2fe95914ff875e0e6d
afriedman <afriedman@yext.com>
2022-12-12 21:33:08 +0000

    Change url pattern

--- src/path.ts
+++ src/path.ts
@@ -1,9 +1,9 @@
 import { StreamOutput } from "./data.ts";
 
 export function GetLocationsPath(doc: StreamOutput) {
-  return "location-" + doc.locale + "-" + doc.name;
+  return "location-" + doc.locale + "-" + doc.name + "-changed";
 };
 
 export function GetAboutPath(doc: StreamOutput) {
-  return "about-" + doc.locale;
+  return "about-" + doc.locale + "-changed";
 };
