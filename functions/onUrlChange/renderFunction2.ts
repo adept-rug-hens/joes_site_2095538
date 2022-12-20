712763c8330c11d8fd1825e3d3d79d42bb84eda7
afriedman <afriedman@yext.com>
2022-12-20 22:05:10 +0000

    url change plugin error testing

--- functions/onUrlChange/renderFunction2.ts
+++ functions/onUrlChange/renderFunction2.ts
@@ -1,3 +1,11 @@
-export async function main() {
-    throw "Intentional ON_URL_CHANGE error"
+import { urlWritebackPlugin } from "https://deno.land/x/yextpages@plugins@1.0.0-beta.3/index.ts"
+
+const main = urlWritebackPlugin({
+	apiKey:  "e4a3698b6ee02c74e90f1a6ac524c543",
+	field: "c_pagesURL",
+	v: "20221010"
+})
+
+export {
+	main
 }
