bae63f1fee1042bee61d020c37e5534eff91c5cd
jhelenek <jhelenek@yext.com>
2023-09-26 20:28:37 +0000

    render error(?)

--- src/mod.ts
+++ src/mod.ts
@@ -43,6 +43,6 @@ export async function RenderPage(doc: Data) {
 </body>
 </html>` 
   }
-  const result: Result = { content: template, path: path, redirects: [`other-${path}`] };
+  const result: Result = { content: template };
   return result;
 }
