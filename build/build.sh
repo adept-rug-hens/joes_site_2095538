3298fab2ed38eb4f7c90f04024d7bd5b1bdaa75d
jballschneider <55411969+jballschneider@users.noreply.github.com>
2023-10-03 11:34:00 -0400

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -3,5 +3,5 @@ export PATH="$HOME/.deno/bin:$PATH"
 deno bundle --config tsconfig.json src/index.tsx assets/bundle.js
 
 touch redirects.csv
-echo "/home.html,/index.html" >> redirects.csv
+echo "/csv-home.html,/index.html" >> redirects.csv
 cat redirects.csv
