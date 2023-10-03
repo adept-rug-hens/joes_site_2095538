6a3c3c340abde880eba96cf4b10cbfc78a6b52ed
jballschneider <55411969+jballschneider@users.noreply.github.com>
2023-10-03 09:51:11 -0400

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -2,7 +2,6 @@ curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.18.0
 export PATH="$HOME/.deno/bin:$PATH"
 deno bundle --config tsconfig.json src/index.tsx assets/bundle.js
 
-touch sites-config/redirects.csv
-echo "/home.html,/index.html" >> sites-config/redirects.csv
-ls -al sites-config
-cat sites-config/redirects.csv
+touch redirects.csv
+echo "/home.html,/index.html" >> redirects.csv
+cat redirects.csv
