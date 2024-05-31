305c881986e9a65daddbc5af60a2e22e36ad9bc2
jhelenek <141873056+jhelenek@users.noreply.github.com>
2024-05-31 18:00:37 -0400

    Update pluginTest.ts

--- src/pluginTest.ts
+++ src/pluginTest.ts
@@ -1,3 +1,3 @@
 export async function TestPlugin() {
-	return {"page_performance_result_info":[{"url":"path.com","template":"location","wcag":25,"speed":50,"seo":75}]}
+	return {"page_performance_result_info":[{"url":"path.com","template":"location","accessibility":25,"performance":50,"seo":75}]}
 }
