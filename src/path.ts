dd6ba94aabc723b210603a975dec19739403a2cc
jhelenek <141873056+jhelenek@users.noreply.github.com>
2025-06-03 10:58:57 -0400

    Update path.ts

--- src/path.ts
+++ src/path.ts
@@ -1,8 +1,7 @@
 import { StreamOutput } from "./data.ts";
 
-export async function GetLocationsPath(doc: StreamOutput) {
+export function GetLocationsPath(doc: StreamOutput) {
   console.log(doc.id);
-  await new Promise(f => setTimeout(f, 1000));
 
   return "location-" + doc.locale + "-" + doc.id;
 };
